60d592a8733418d8998bc1c8f1208eb071535fac
darcusco <dario.sim3@gmail.com>
2019-09-10 12:48:01 +0200

    I love it

---
+++ Untitled-3.py
@@ -0,0 +1,28 @@
+#%%
+x = 2
+#%%
+"HEY"*3
+
+string = "testing out"
+string.capitalize
+
+#%%
+def hello():
+    print ("World")
+hello()
+
+#%%
+x = hello
+
+#%%
+x()
+
+#%%
+quote = ("I am a Cowbot"+" and "+"I am about to get shot. ")*3
+quote.capitalize()
+print(f"{quote[0:10]}")
+#%%
+def printfun(x,y):
+    print(f"{x} + {y} = {x+y}")
+printfun(2,45)
+#%%
